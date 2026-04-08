2b081a908271d029294ec1e71ad35eebaec14c20
Shivam Kumar <shivam.ku@browserstack.com>
2026-04-08 09:39:06 +0530

    fix-2

--- junit-4/android/src/test/java/com/browserstack/FirstTest.java
+++ junit-4/android/src/test/java/com/browserstack/FirstTest.java
@@ -10,28 +10,14 @@ import io.appium.java_client.AppiumBy;
 
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
 public class FirstTest extends BrowserStackJUnitTest {
 
   @Test
   public void test() throws Exception {
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-
-    // Try accessibilityId first; fall back to the search container view id
-    // (Wikipedia Alpha's search bar content-desc changed in some app versions)
-    WebElement searchElement = wait.until(driver -> {
-      try {
-        WebElement el = driver.findElement(AppiumBy.accessibilityId("Search Wikipedia"));
-        if (el.isDisplayed()) return el;
-      } catch (NoSuchElementException ignored) {}
-      try {
-        WebElement el = driver.findElement(AppiumBy.id("org.wikipedia.alpha:id/search_container"));
-        if (el.isDisplayed()) return el;
-      } catch (NoSuchElementException ignored) {}
-      return null;
-    });
+    WebElement searchElement = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
+        ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Search Wikipedia")));
     searchElement.click();
     WebElement insertTextElement = (WebElement) new WebDriverWait(driver, Duration.ofSeconds(30)).until(
         ExpectedConditions.elementToBeClickable(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")));
